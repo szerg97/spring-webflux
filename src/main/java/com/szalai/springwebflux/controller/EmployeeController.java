7a88ff31c14d3b04690221eef13974dbcecf4059
Gergo Szalai <szerg97@gmail.com>
2023-04-03 12:47:56 +0200

    updated request mapping for controller

--- src/main/java/com/szalai/springwebflux/controller/EmployeeController.java
+++ src/main/java/com/szalai/springwebflux/controller/EmployeeController.java
@@ -8,7 +8,7 @@ import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
 @RestController
-@RequestMapping("/employees")
+@RequestMapping("/api/v1/employees")
 @RequiredArgsConstructor
 public class EmployeeController {
 
